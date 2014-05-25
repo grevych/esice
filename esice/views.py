cb9ae40cc739f6d9cdad90e5b1e27a1edf56d628
Gerardo Reyes <grevych@gmail.com>
2014-05-25 17:15:59 -0500

    Meta for google verification

--- esice/views.py
+++ esice/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def home(request):
 
-	return HttpResponse(
-    		'Hola', 
-    		status=200
-	)
+    template_name = 'google_verification.html'
+
+    return render(
+        request,
+        template_name,
+        {}
+    )
